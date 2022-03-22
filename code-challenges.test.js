e8957b8a800cf73da4fa55e78cf296ae9cf96dcb
Nathaniel Bethea <jumpnate23@outlook.com>
2022-03-22 09:42:18 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,47 +10,130 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
+const secretCodeWord1 = "Lackadaisical";
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
+const secretCodeWord2 = "Gobbledygook";
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
+const secretCodeWord3 = "Eccentric";
 // Expected output: "3cc3ntr1c"
 
+// Pseudo Code
+// create a describe statement with name of function
+// create an in it statement describing what the method does
+// create expect statements for what needs to be tested
+
+describe("codedMessage", () => {
+  it("returns a coded message", () => {
+    expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l");
+    expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k");
+    expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c");
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code
+// create const of what we want to replace
+// create const codedMessage
+// create a function that takes in a string of vowels
+// return values
+
+const code = {
+  a: 4,
+  e: 3,
+  i: 1,
+  o: 0,
+  A: 4,
+  E: 3,
+  I: 1,
+  O: 0,
+};
+
+const codedMessage = (str) => {
+  return str.replace(/[aeioAEIO]/gi, (string) => code[string]);
+};
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
-const letterA = "a"
+const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"];
+const letterA = "a";
 // Expected output: ["Apple", "Banana", "Orange"]
-const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
-const letterE = "e"
+const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"];
+const letterE = "e";
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+// Pseudo Code
+// create a describe statement with name of function
+// create an in it statement describing what the method does
+// create expect statements for what needs to be tested
+
+describe("particularLetter", () => {
+  it("takes in an array of words and a single letter and returns all the words that contain that particular letter", () => {
+    expect(particularLetter(arrayOfWords1, letterA)).toEqual([
+      "Apple",
+      "Banana",
+      "Orange",
+    ]);
+    expect(particularLetter(arrayOfWords2, letterE)).toEqual([
+      "Cherry",
+      "Blueberry",
+      "Peach",
+    ]);
+  });
+});
 
 // b) Create the function that makes the test pass.
 
+// Pseudo Code
+// create const particularLetter
+// create a function that takes in an array and letter
+// use .filter method, .toLowerCase and .includes notation
+// return values
+
+const particularLetter = (array, letter) => {
+  return array.filter((value) => value.toLowerCase().includes(letter));
+};
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+const hand1 = [5, 5, 5, 3, 3];
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
+const hand2 = [5, 5, 3, 3, 4];
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
+const hand3 = [5, 5, 5, 5, 4];
 // Expected output: false
 
+// Pseudo Code
+// create a describe statement with name of function
+// create an in it statement describing what the method does
+// create expect statements for what needs to be tested
+
+describe("fullHouseTester", () => {
+  it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+    expect(fullHouseTester(hand1)).toEqual(true);
+    expect(fullHouseTester(hand2)).toEqual(false);
+    expect(fullHouseTester(hand3)).toEqual(false);
+  });
+});
 
 // b) Create the function that makes the test pass.
+
+// Pseudo Code
+// create const fullHouseTester
+// create a function that takes in an array
+// use .map to pull in values
+// use . filter
+// create a return of the hand counts
+
+const fullHouseTester = (arr) => {
+  let handCount = arr.map((v, i, arr) => arr.filter((b) => b === v).length);
+  return handCount.includes(3) && handCount.includes(2);
+};
--- interview-questions.md
+++ interview-questions.md
@@ -1,4 +1,5 @@
 # ASSESSMENT 5: Interview Practice Questions
+
 Answer the following questions.
 
 First, without external resources. Challenge yourself to answer from memory.
@@ -7,53 +8,44 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a block in Ruby?
 
-  Your answer:
+Your answer: A function that can be passed to a method
 
-  Researched answer:
-
-
+Researched answer: Anonymous functions that can be passed into methods. Blocks can also have arguments
 
 2. What is a gem?
 
-  Your answer:
+Your answer: A ruby plugin
 
-  Researched answer:
-
-
+Researched answer: A package manager for Ruby that gives a standard format for Ruby programs and libraries
 
 3. What is Ruby on Rails?
 
-  Your answer:
+Your answer: A web application dev framework
 
-  Researched answer:
-
-
+Researched answer: An MVC framework that provides default structures for a database, a web service, and web pages.
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+Your answer: An SQL database where you can edit data
 
-  Researched answer:
-
-
+Researched answer: A type of database that stores and provides access to data points that are related, Object-oriented databases, Key-value databases, and Hierarchical databases.
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+Your answer: A Specific choice of attributes which is used as an ID
 
-  Researched answer:
-
-
+Researched answer: A special relational database table column that is used to id each table record
 
 ## Looking Ahead: Terms for Next Week
+
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: A route that gives mapping from HTTP to CRUD actions
 
-2. JSON:
+2. JSON: An open file format and data interchange format that uses text to store and transmit data objects
 
-3. ERB:
+3. ERB: A template system that embeds Ruby into a text document
 
-4. Params:
+4. Params: a method that when used properly returns an ActionController::Parameters object
 
-5. API:
+5. API: A connection computers make with computer programs, that is also a type of software interface
